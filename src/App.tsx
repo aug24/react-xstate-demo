fdf2671066494af11896547c87cfe87e8924bbdc
Justin Rowles <justin.rowles@ovo.com>
2025-06-24 08:45:41 +0100

    Expose state and value for debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import {useMemo} from 'react'
+import {useEffect, useMemo} from 'react'
 import './App.css'
 import {createActor} from "xstate";
 import {counterMachine} from "./counterMachine.tsx";
 import {useSelector} from "@xstate/react";
 import {IncrementButton} from "./components/IncrementButton.tsx";
 import {DecrementButton} from "./components/DecrementButton.tsx";
+import type {CounterContext} from "./counter/Counter.tsx";
+
+type WindowExpose = { state: { context: CounterContext, value: string } }
 
 function App() {
 
@@ -18,6 +21,14 @@ function App() {
     const name = useSelector(actor, (state) => state.context.name);
     const error = useSelector(actor, (state) => state.context.error);
     const value = useSelector(actor, (state) => state.value);
+    const state = useSelector(actor, (state) => state);
+
+    useEffect(() => {
+        (window as unknown as WindowExpose).state = {
+            context: state.context,
+            value: state.value.toString()
+        }
+    }, [state]);
 
     return (
         <>
@@ -26,9 +37,9 @@ function App() {
                 <IncrementButton onClick={() => actor.send({type: 'INCREMENT'})}/>
                 <DecrementButton onClick={() => actor.send({type: 'DECREMENT'})}/>
                 <input id="name" onChange={(e) => {
-                        console.log("sending", e.target.value)
-                        actor.send({type: 'CHANGE', id: e.target.id, value: e.target.value})
-                    }
+                    console.log("sending", e.target.value)
+                    actor.send({type: 'CHANGE', id: e.target.id, value: e.target.value})
+                }
                 }
                        value={name}/>
                 <p>{value.toString()}</p>
